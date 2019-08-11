dd7596ee6609ab78dcab75aeeb0cfb6bd80ca6ce
Anja <rasoloanja@gmail.com>
2019-08-11 12:10:09 -0400

    Init app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Title from './components/Title';
+import Form from './components/Form';
+import Weather from './components/Weather';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+          <Title />
+          <Form />
+          <Weather />
+      </div>
+    )
+  }
 }
+
 
 export default App;
---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const title = ()  => {
+    return (
+        <div>
+            <h1>Weather Finder</h1>
+            <p>Find out temperature, conditions and more...</p>
+        </div>
+    )
+}
+
+export default title
