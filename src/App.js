c5f99038db5d9775b35d891bcb49ed753a1ac980
Anja <rasoloanja@gmail.com>
2019-08-11 19:30:01 -0400

    Styled cities list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import './Flag.css';
+
+
 import Title from './components/Title/Title';
 import Form from './components/Form/Form';
 import SingleWeather from './components/SingleWeather/SingleWeather';
-import flag from './assets/country-flags/svg/001-ethiopia.svg';
+
+
+
+
 
 
 const API_KEY = '2287b09e1620e5a36dcd2833009015f0';
@@ -34,9 +40,6 @@ class App extends Component {
     const data = await api_call.json();
 
     console.log(data)
-
-    
-
 
     if(city){
       this.setState({
@@ -70,8 +73,43 @@ class App extends Component {
                           country={this.state.country}
                           humidity={this.state.humidity}
                           description={this.state.description}/>
+
+      <div className="list__cities">      
+       {
+           this.state.citiesList.length > 0 && this.state.citiesList.map( city => {
+             return (
+               <div key={city.id}
+                    className="city">
+
+                  <div className="city__weather">
+                      <div className="city__weather__img"               
+                            style={{backgroundImage: `url("http://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png")`,
+                            backgroundPosition: "center",
+                            backgroundRepeat: "no-repeat",
+                            backgroundSize: "cover",
+                      }} />
+                       <p className="city__weather__desc">{city.weather[0].description}</p>
+                  </div>
+
+                  <div className="city__temp">
+                      <p>{city.main.temp} &#8451;</p>
+                  </div>
+
+                  <div className="city__details">
+                      <p className="city__details__name">{city.name}, {city.sys.country} <span className={`flag flag-${city.sys.country.toLowerCase()}`}></span></p>
+                      <p className="city__details__data">From {city.main.temp_min} to {city.main.temp_max} &#8451;</p>
+                      <p className="city__details__data">Lat: {city.coord.lat} - Long: {city.coord.lon}</p>
+                  </div>
+                  
+               
+                  
+               </div>
+             )
+           })
+         }       
+
+        </div>
        
-         
       </div>
     )
   }
