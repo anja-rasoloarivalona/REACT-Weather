e9446572066c6a9c16654d6a5ca5ef0d789bc8f9
Anja <rasoloanja@gmail.com>
2019-08-12 21:36:24 -0400

    Styled weather forecast section of single weather component

--- src/components/SingleWeather/SingleWeather.js
+++ src/components/SingleWeather/SingleWeather.js
@@ -4,31 +4,98 @@ import './SingleWeather.css';
 
 const API_KEY = '2287b09e1620e5a36dcd2833009015f0';
 
+const WEEKDAY = new Array(7)
+
+WEEKDAY[0] = "MON";
+WEEKDAY[1] = "TUE";
+WEEKDAY[2] = "WED";
+WEEKDAY[3] = "THU";
+WEEKDAY[4] = "FRI";
+WEEKDAY[5] = "SAT";
+WEEKDAY[6] =  "SUN";
 
 class SingleWeather extends Component {
 
     state = {
-        weather: {}
+        weather: undefined,
+        forecast: undefined
     }
 
 
-    componentWillMount = async () =>{
+    componentDidMount = async () =>{
         const api_call = await fetch(
             `http://api.openweathermap.org/data/2.5/forecast?q=${this.props.city},${this.props.country.toLowerCase()}&appid=${API_KEY}&units=metric`
           );
         let weatherData = await api_call.json();
 
         if(this.props.city && this.props.country){
+        
+            let results = [];
+
+            /*List of forecasts for the coming days*/
+            let data = weatherData.list;
+
+            /* Keep the forecasts data where hour is equal to 12:00:00 and store them in newData */
+            let newData = data.filter( i => {
+              return i.dt_txt.slice(11) === '12:00:00'
+            })
+            
+            /* Store the day (in weekday format), temperature and icon datas into the results array for each forecast remaining */
+        
+            newData .map( i => {
+                    let test = i.dt_txt.slice(0, 10);
+                    let newTest = (new Date(test)).getDay(); 
+
+                    let dayInLetter = WEEKDAY[newTest];
+                    let temp = Math.round(i.main.temp);
+                    let icon = i.weather[0].icon;
+                    
+                    let forecastDataByDate = {
+                        day: dayInLetter,
+                        temp: temp,
+                        icon: icon
+                    }
+
+                results = [...results, forecastDataByDate];
+            
+            })
+
             this.setState({
-                weather: weatherData
-            }, () => console.log('from single weather forecast', this.state.weather))
+                weather: weatherData,
+                forecast: results
+            }, () => console.log('biim',this.state.forecast))
         }
       
     }
 
 
     render() {
+
+
         let image = require(`../../assets/weather/${this.props.icon}.jpg`)
+
+        let forecast;
+
+        if(this.state.forecast){
+            let data = this.state.forecast;
+
+            forecast = data.map( i => (
+                <div className="single-weather__forecast"
+                     key={i.day}>
+
+                    <div className="single-weather__forecast__day">{i.day}</div>
+                    <div className="single-weather__forecast__img"               
+                            style={{backgroundImage: `url("http://openweathermap.org/img/wn/${i.icon}@2x.png")`,
+                            backgroundPosition: "center",
+                            backgroundRepeat: "no-repeat",
+                            backgroundSize: "cover",
+                      }} />
+                    <div className="single-weather__forecast__temp" >
+                        {i.temp} &#176;
+                    </div>
+                </div>
+            ))
+        }
 
     return (
 
@@ -61,6 +128,11 @@ class SingleWeather extends Component {
                         <p>Humidity: {this.props.humidity} %</p>
                         <p>Wind: {this.props.wind} m/s</p>
                    </div>
+
+                    <div className="single-weather__forecast__container">
+                        {forecast} 
+                    </div>
+                          
                     
                     
                 </section>
